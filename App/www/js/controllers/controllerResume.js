f4be2a8a60abf60491ea25f50058207b762a1139
Alex <ouatu.alexandru@gmail.com>
2017-03-01 16:44:32 +0100

    datepicker

--- App/www/js/controllers/controllerResume.js
+++ App/www/js/controllers/controllerResume.js
@@ -1,4 +1,4 @@
-angular.module('starter').controller('ResumeCtrl', function($scope, InfoFactories, $state, $ionicLoading, WebService) {
+angular.module('starter').controller('ResumeCtrl', function($timeout, $cordovaDatePicker, $scope, InfoFactories, $state, $ionicLoading, WebService) {
     $scope.locale = window.locale;
     $scope.selectedParking = InfoFactories.getPark();
     $scope.selectedClient = InfoFactories.getClientSelected();
@@ -20,10 +20,6 @@ angular.module('starter').controller('ResumeCtrl', function($scope, InfoFactorie
         InfoFactories.setTelepass($scope.hasTelepass);
     };
     
-    $scope.changeDate = function() {
-        $state.go('tab.newbooking');
-    };
-    
     $scope.changeParking = function() {
         $state.go('tab.parking');
     };
@@ -34,4 +30,48 @@ angular.module('starter').controller('ResumeCtrl', function($scope, InfoFactorie
         }
         $state.go('tab.selcar');
     };
+
+    var dateFromConfig = {
+        date: $scope.dateFromPick ? $scope.dateFromPick : new Date(),
+        mode: 'date',
+        allowOldDates: false,
+        allowFutureDates: true,
+        doneButtonLabel: $scope.locale.date.butChange,
+        cancelButtonLabel: $scope.locale.date.labelClose,
+        cancelButtonColor: '#000000',
+        locale: $scope.locale.locale
+    };
+
+    var timeFromPick = {
+        date: $scope.timeFromPick ? $rootScope.timeFromPick.inputTime : new Date(),
+        mode: 'time',
+        is24Hour: true,
+        allowOldDates: true,
+        allowFutureDates: true,
+        doneButtonLabel: $scope.locale.date.butChange,
+        cancelButtonLabel: $scope.locale.date.labelClose,
+        cancelButtonColor: '#000000',
+        locale: $scope.locale.locale
+    };
+
+    function startTimePicker (){
+        $cordovaDatePicker.show(timeFromPick).then(function(time) {
+            $scope.timeFromPick = time;
+                });
+    }
+
+    $scope.selectFromDateTime = function(){
+        
+        $cordovaDatePicker.show(dateFromConfig).then(function(date) {
+            if(date){
+                $scope.dateFromPick = date;
+                $timeout(function() {
+                    startTimePicker();
+                }, 1000);
+            }else{
+
+            }
+            
+        });
+    }
 })
