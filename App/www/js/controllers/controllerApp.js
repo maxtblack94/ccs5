eb4b49282b56b50fd368209c9899f81783ddb240
Alex <ouatu.alexandru@gmail.com>
2017-05-16 23:59:06 +0200

    [CROSS] sidemenu + tabs v3

--- App/www/js/controllers/controllerApp.js
+++ App/www/js/controllers/controllerApp.js
@@ -1,3 +1,13 @@
-angular.module('starter').controller('AppCtrl', function($state, $scope) {
-   $scope.currentState = ($state.currentState || {}).name;
+angular.module('starter').controller('AppCtrl', function($rootScope, $scope, $state) {
+   $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $scope.currentState = toState.name;
+    })
+
+    $scope.changeStateContacts = function(){
+        if($scope.currentState !== "contacts"){
+            $state.go("contacts");
+        }else{
+            $state.go("tab.bookings");
+        }
+    }
 })
--- App/www/js/controllers/controllerClientDetails.js
+++ App/www/js/controllers/controllerClientDetails.js
@@ -1,12 +1,12 @@
-angular.module('starter').controller('ClientDetailCtrl', function($state, $scope, InfoFactories) {
+angular.module('starter').controller('ClientDetailCtrl', function($state, $scope, InfoFactories, PopUpServices, $ionicLoading, ScriptServices) {
     $scope.locale = window.locale;
     $scope.selectedClient = InfoFactories.getClientSelected();
-    $scope.userInfo = InfoFactories.getUserInfo().registry;
+    $scope.userInfo = InfoFactories.getUserInfo();
 
     $scope.edit = function(){
         $scope.request = {
-            "email": $scope.userInfo.email,
-            "cellulare": $scope.userInfo.mobile_phone
+            "email": (($scope.userInfo || {}).registry || {}).email,
+            "cellulare": (($scope.userInfo || {}).registry || {}).mobile_phone
         }
     }
 
@@ -15,7 +15,32 @@ angular.module('starter').controller('ClientDetailCtrl', function($state, $scope
     }
 
     $scope.save = function(){
-        $scope.request = undefined;
+        if(!$scope.request.email){
+            PopUpServices.errorPopup("Tutti i campi obblocatori", "1");
+       }else{
+           callSaveService();
+       }
+    }
+
+    function callSaveService(){
+        $ionicLoading.show();
+        ScriptServices.getXMLResource(558).then(function(res) {
+            res = res.replace('{DRIVERNUMBER}', InfoFactories.getUserInfo().driverNumber)
+            .replace('{PHONE}', $scope.request.cellulare)
+            .replace('{EMAIL}', $scope.request.email)
+            .replace('{SMS}', $scope.userInfo.sms)
+            .replace('{PUSH}', $scope.userInfo.push);
+            ScriptServices.callGenericService(res, 558).then(function(data) {
+                $scope.request = undefined;
+                $scope.userInfo.registry = data.data;
+                window.localStorage.setItem('userInfo', JSON.stringify($scope.userInfo));
+                $ionicLoading.hide();
+                PopUpServices.messagePopup("Contatti modificati!", "Successo");
+            }, function(error) {
+                $ionicLoading.hide();
+                PopUpServices.errorPopup("Non è stato possibile modificare i contatti");
+            })
+        });
     }
 
 })
