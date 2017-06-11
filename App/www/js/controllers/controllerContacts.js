faf5989e4a37d477be47cebe75de8355d50174fa
Alex <ouatu.alexandru@gmail.com>
2017-06-11 14:33:33 +0200

    [CROSS] mail send + fix tel + configuration proximity

--- App/www/js/controllers/controllerContacts.js
+++ App/www/js/controllers/controllerContacts.js
@@ -2,6 +2,15 @@ angular.module('starter').controller('ContactsCtrl', function(InfoFactories, $sc
    $scope.locale = window.locale;
    $scope.clientContacts = InfoFactories.getClientSelected().contact;
    $scope.startCall = function(number){
-        window.open("tel:" + number);
+        window.open("tel:" + number.replace(/\s+/g, ''), "_system");
+   }
+
+   $scope.openEmail = function(mail){
+       if(cordova.plugins.email){
+            cordova.plugins.email.open({
+                to:      mail,
+                subject: 'CCS Richiesta supporto #'+new Date().getTime()
+            });
+       }
    }
 })
