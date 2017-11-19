912ba552c8400f3708f14dea0914c1a8a09b2900
Alexandru Gabriel Ouatu <alexandru.g.ouatu@avanade.com>
2017-11-19 19:19:45 +0100

    fix

---
+++ App/www/js/controllers/controllerSettings.js
@@ -0,0 +1,33 @@
+angular.module('starter').controller('SettingsCtrl', function($rootScope, $scope, $http, $state, $ionicLoading, InfoFactories, WebService) {
+    $scope.locale = window.locale;
+    $scope.selectedClient = InfoFactories.getClientSelected();
+    
+    $scope.setHasPicture = function() {
+        $rootScope.hasPicture = !$rootScope.hasPicture;
+        window.localStorage.setItem('hasPicture', $rootScope.hasPicture);
+    };
+    
+    $scope.logout = function(action) {
+        var driverNumber = InfoFactories.getUserInfo().driverNumber;
+        window.localStorage.removeItem('userInfo');
+        $ionicLoading.show();
+        $http.get("res/569.xml").success(function(res) {
+            $ionicLoading.hide();
+            res = res.replace('{USER_ID}', driverNumber);
+            WebService.ajaxPostRequest(res, 569, null);
+            InfoFactories.resetService();
+            if(action==='clearClient'){
+                window.localStorage.removeItem('selectedClient');
+            }
+            $state.go('login');
+        });
+    };
+
+    $scope.deleteClienteContext = function(){
+        if(counter===4){
+            $scope.logout('clearClient');
+        }else{
+            counter++
+        }
+    }
+})
