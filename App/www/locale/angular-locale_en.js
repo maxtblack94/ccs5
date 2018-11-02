0475ee5955b8faa914330f24b0f612a78d9a67fb
Alex <ouatu.alexandru@gmail.com>
2018-11-02 04:21:39 +0100

    lingua inglese e rumena con le label nuove

--- App/www/locale/angular-locale_en.js
+++ App/www/locale/angular-locale_en.js
@@ -1,153 +1,233 @@
 window.locale_en = {};
 
-window.locale_en.locale = 'en-EN';
-
-window.locale_en.menu = {
+window.locale_en.leftMenu = {
+	menuTitle: 'User Profile',
+	username: 'Username',
+	editPassword: 'Change Password',
+	menuDivider: 'Menu',
 	home: 'Home',
-	date: 'Date',
-	parking: 'Parking',
-	booking: 'Booking',
-	vehicles: 'Vehicle'
+	driveLicense: 'License Data',
+	settings: 'Settings',
+	help: 'Help',
+	reserveHistory: 'Booking history',
+	logout: 'Logout'
+}
+
+window.locale_en.sos = {
+	title: 'SOS',
+	info: 'The SOS section gives you 24H support. It is important to use the service only in the event of a real emergency! ',
+	sendRequest: 'Send SOS Request'
+}
+
+window.locale_en.bookings = {
+	title: 'Reservations',
+	notFound: 'You currently have no current bookings',
+	pnr: 'PNR',
+	plate: 'Targa',
+	color: 'Color',
+	seats: 'Places',
+	externalClean: 'External',
+	internalClean: 'Interni',
+	get: 'Withdrawal',
+	set: 'Returning',
+	openCar: 'Start running',
+	closeCar: 'End of run',
+	cancelReservation: 'Cancel'
+}
+
+window.locale_en.parking = {
+	title: 'Choose parking',
+	noparking: 'No parking found',
+	placeholderSearch: 'Search'
 };
 
 window.locale_en.tabs = {
 	reservations: 'Reservations',
-	park: 'Park',
-	booking: 'Booking',
+	park: 'Parkings',
+	booking: 'Book',
 	vehicles: 'Vehicles',
-	settings: 'Settings'
+	settings: 'Settings',
+	sos: 'SOS',
+	notifications: 'Notifications'
 };
 
-window.locale_en.home = {
-	msgHello: "Hello, {0}",
-	msgNoRsv: 'You have no reservation in progress',
-	msgNextRsv: 'Hai una prenotazione il {0} alle ore {0} ',
-	msgOnRsv : "Devi riconsegnare l'auto alle ore {0}",
-	labelCar: 'Vehicle',
-	labelGet: 'Pick-Up',
-	labelSet: 'Return',
-	butBooking: 'New booking',
-	labelCountdown: 'Countdown',
-	labelPlate: 'Plate',
-	cancelReservation: 'Cancel',
-	labelColor: 'Color',
-	confirmDeleteBook: 'Revoke selected booking?',
-	title: 'Bookings',
-	prenotationtitle: 'Booking detail',
-	vehicle_position: 'Vehicle position address'
+window.locale_en.notifications = {
+	title: 'Notifications',
+	notFound: 'There are no notifications ..'
 };
 
-window.locale_en.home.rsvinfo = {
-	labPRN: "PRN ",
-	labPrk: 'Parking ',
-	labTime: '',
+window.locale_en.commons = {
+	refresh: 'Update ...',
+	yes: 'Yes',
+	no: 'No',
+	legalNotice: 'Copyright © 2016 Corporate Car Solutions Italy All Rights Reserved',
+	readMore: 'Continue reading ...',
+	search: 'Search ..',
+	select: 'Select',
+	close: 'Close',
+	attention: 'Attention',
+	insert: 'Insert',
+	alert: 'Alert',
+	success: 'Success',
+	error: 'Error',
+	connectionProblem: 'We encountered a connection problem!',
+	successOperation: "Operation successful",
+	requestFail: "We could not submit the request.",
+	refuse: 'Reject',
+	accept: 'Accept'
+};
+
+window.locale_en.bookResume = {
+	title: 'Book',
+	parking: 'Parking',
+	parkOpenedFrom: 'Open from',
+	parkOpenedTo: 'Alle',
+	parkOpened24h: 'Open H24',
+	dateFrom: 'Collection date',
+	dateTo: 'Delivery date',
+	cc: 'Fuel Card',
+	cc_descr: 'Select if you need the fuel card',
+	drivingRange: 'Travel radius',
+	telepass: 'Telepass',
+	telepass_descr: 'Select if you need the telepass',
+	vahicleType: 'Vehicle type',
+	confirm: 'Search for vehicle',
+	returnDateIsOut: 'Collection date is outside the parking hours',
+	returnDateIsMajor: 'Collection date is greater than delivery date',
+	returnDateNeedToBeMajor: 'Collection date must be higher than current date',
+	isNotInTime: 'The delivery date is outside the parking hours',
+	defineRange: 'You have not defined the radius of travel',
+	missingVehicleType: 'Select a vehicle type',
+	vehicleTypesNotFound: 'It was not possible to recover the types of vehicles in this car park!',
+	noDataFound: 'Some information could not be retrieved!',
+	returnDateHaveToBe: 'The return date must be before',
+	wrongDates: 'Select collection and delivery date to continue',
+	wrongParking: 'Select parking before continuing'
+};
+
+window.locale_en.map = {
+	title: 'Reservation detail',
+	carLocation: 'Vehicle position address',
+	get: 'Withdrawal',
+	set: 'Returning',
+	goTo: 'Reach vehicle',
+	delete: 'Delete',
+};
+
+window.locale_en.confirmReservation = {
+	title: 'Booking Confirmation',
+	back: 'Back',
+	confirm: 'Confirm',
+	wait: 'wait ..',
+	requestComplete: 'Confirmed Reservation',
+	seats: "Places",
+	color: "Color",
+	incomplete: 'Warning',
+	park: 'Parking',
+	selectPlace: "Enter the 'Place'",
+	selectDriveName: "Enter 'Driver Name'",
+	dateFrom: 'Collection date',
+	dateTo: 'Delivery date',
+	cc: 'Fuel Card',
+	telepass: 'Telepass',
+	pnr: 'PNR'
+};
+
+window.locale_en.vehicle = {
+	title: 'Select vehicle',
+	notFound: 'We\'re sorry, all vehicles are booked for the period you requested.',
+	plate: 'Targa',
+	select: 'Select',
+	seats: 'Places',
+	color: 'Color',
+	externalClean: 'External',
+	internalClean: 'Interni',
+	labelCannotReserve: 'Your reservation can not be accepted, as there is already a reservation in your name for the requested period.'
 };
 
 window.locale_en.login = {
+	username: 'Username',
+	password: 'Password',
 	login: 'Login',
-	forgotPassword: 'Forgot password?',
-	retrievePassword: 'Retrieve Password',
-	haveAccount: 'I have account',
-	placeholderMail: 'company@domain.com'
+	forgotPassword: 'Password forgotten?',
+	retrievePassword: 'Recover Password',
+	haveAccount: 'Login',
+	email: 'Email',
+	placeholderEmail: 'compagnia@dominio.it',
+	companyCode: 'Verify company code',
+	companyCodePlaceholder: 'Enter company code'
 };
 
-window.locale_en.parking = {
-	button: 'Next',  
-	labelSearch: 'Search parking..',
-	labelSelectParking: 'Select parking',
-	noparking: 'No parkings found',
-	placeholderSearch: 'Search'
-};
-
-
-window.locale_en.vehicle = {
-	buttonBook:'Reserve',
-	buttonConfirm: '',
-	title: 'Vehicles',
-	messageEnpty: 'No available vehicles',
-	label: 'Vehicle',
-	labelPlate: 'Plate',
-	labelMotor: 'Motor',
-	labelSeats: 'Seats',
-	labelExternal: 'Exterior',
-	labelInterior: 'Interior',
-	labelColor: 'Color',
-	yes: 'YES',
-	no: 'NO',
-	labelSelect: "Select",
-    labelCannotReserve: 'Your booking request cannot be accepted, due to an existing one in the same period at your name.'
-};
-
-window.locale_en.booking = {
-	buttonDate: 'Change',
-	buttonParking: 'Change',
-	messageEmptyDate: 'There is something wrong in date.',
-	buttonConfirm: 'Search Vehicle',
-	dateFrom : 'Pickup date',
-	dateTo: 'Return date',
-	alertMessage: 'Select Data Properly',
-	parking: 'Parking',
-	justification: 'Justification',
-	place: 'Place',
-	cc: 'Fuel card',
-	telepass: 'Telepass',
-	telepass_descr: 'Select telepass',
-	labelDelete: 'Revoke booking',
-	wrongInputs: 'Please select correctly pick-up, return date and parking',
-	header: "Select ",
-	drivingRange: 'Driving range',
-	driverName: 'Driver name',
-	labelNavigate: 'Reach vehicle',
-	buttonDetail: 'Details',
-	buttonOpenCar : 'Open',
-	buttonCloseCar : 'Close',
-	labelCentralizzedButton: "Open/Close Vehicle"
-};
-
-window.locale_en.confirmation = {
-	title: 'Confirm reservation',
-	back: 'Cancel',
-	buttonConfirm: 'Confirm',
-	labelWait: 'please wait..',
-	labelRequestComplete: 'Reservation confirmed',
-	labelSeat: "Seats",
-	labelColor: "Color",
-	labelIncomplete: 'Attention',
-	labelSelectPlace: "Insert 'Place'",
-	labelSelectDriveName: "Insert 'Driver name'"
-};
-
-window.locale_en.date = {
-	startDate: 'Pick-Up date',
-	startTime: 'Pick-Up time',
-	endDate: 'Return date',
-	endTime: 'Return time',
-	button: 'Next',
-	change: 'Change',
-	messageDate: 'Please select date Properly',
-	messageTime: 'Please select time Properly',
-	messageAlert: 'Please select Properly',
-	butChange: 'Change',
-	labelSelectRange: 'Select booking date range',
-	labelIncorrectDate: 'Date is not correct',
-	labelSmallDate: "Pick-Up Time can't be less then current time",
-	labelToday: "Today",
-	labelClose: "Close",
-	monthList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
-	weekDaysList: ["Sun", "Mon", "Tue", "Wed", "thu", "Fri", "Sat"]
-};
-
-window.locale_en.account = {
+window.locale_en.settings = {
+	personalData: 'Personal data',
+	client: 'Customer',
 	title: 'Settings',
-	check1: 'Small list',
-	client: 'Client',
-	logout: 'Logout',
-	check1_descr: 'Show list with small pictures'
+	check1: 'Minimize Images',
+	check1_descr: 'Show reduced vehicle images',
+	editPassword: 'Change password'
 };
 
-window.locale_en.resume = {
-	title: 'Resume',
-	wrongDates: 'Select pick-up and return date to continue',
-	wrongParking: 'Select parking to continue'
+window.locale_en.driveLicense = {
+	title: 'Driving License',
+	license: 'License',
+	number: 'License Number',
+	issuingCountry: 'Place of issue',
+	issuingDate: 'Release date',
+	expiringDate: 'Expiry Date',
+	editLicense: 'Change patent data',
+	cancel: 'Cancel',
+	save: 'Save'
 };
 
+window.locale_en.editPassword = {
+	title: 'Change Password',
+	currentPassword: 'Enter current password',
+	newPassword: 'Enter new password',
+	repeatPassword: 'Confirm new password',
+	cancel: 'Cancel',
+	edit: 'Edit'
+};
+
+window.locale_en.contacts = {
+	title: 'Contacts',
+	email: 'Email',
+	phone: 'Telephone',
+	time: 'Hours'
+};
+
+window.locale_en.clientDetails = {
+	title: 'Personal data',
+	anagrafic: 'Anagrafica',
+	firstName: 'Name',
+	lastName: 'Surname',
+	username: 'Username',
+	badge: 'BadgeID',
+	contacts: 'Contacts',
+	email: 'Email',
+	phone: 'Mobile',
+	editContacts: 'Edit contacts',
+	cancel: 'Cancel',
+	save: 'Save'
+};
+
+window.locale_en.actionSheet = {
+	chageDriver: 'Change Guide',
+	selectEmployee: 'Select employee',
+	confirm: 'Confirm',
+	cancel: 'Cancel',
+	continue: 'Continue',
+	Reporting: 'Reporting',
+	attention: 'Attention',
+	damageInfo1: 'Perform Fault Reporting, only after having ascertained, that the vehicle is unable to return to the parking lot and needs to be brought or transferred to a repair shop.',
+	damageInfo2: 'The Fault Reporting, therefore, entails the closure of the present race, so as to allow them to reserve another vehicle if desired. To close this race, it is requested to communicate moreover ',
+	damageInfo3: 'Please note that once the fault has been reported the vehicle will no longer be usable',
+	totalKM: 'Total kilometers shown on the dashboard',
+	noteWithDesc: 'Notes (useful for the diagnosis of the problem)',
+	needSupport: 'If the vehicle needs to be towed or not',
+	notes: 'Notes',
+	chooseDefectType: 'Choose fault type',
+	fuelLevel: 'Select the fuel level in the tank',
+	notesPlaceholder: 'Any useful information',
+	kmPlaceholder: 'Type the mileage',
+	mechaninc: 'Vehicle towed in the workshop?'
+};
--- App/www/locale/angular-locale_ro.js
+++ App/www/locale/angular-locale_ro.js
@@ -1,153 +1,233 @@
 window.locale_ro = {};
 
-window.locale_ro.locale = 'en-EN';
+window.locale_ro.leftMenu = {
+	menuTitle: 'Profilul utilizatorului',
+	username: "Utilizator",
+	editPassword: "Schimbati parola",
+	menuDivider: "Meniu",
+	home: "Acasa",
+	driveLicense: "Date de licenta",
+	settings: "Setari",
+	help: "Ajutor",
+	reserveHistory: "Istoric rezervari",
+	logout: "Logout"
+}
 
-window.locale_ro.menu = {
-	home: 'Home',
-	date: 'Date',
-	parking: 'Parking',
-	booking: 'Booking',
-	vehicles: 'Vehicle'
+window.locale_ro.sos = {
+	title: "SOS",
+	info: "Sectiunea SOS va ofera suport pentru 24h. Este important sa folositi serviciul numai in cazul unei urgente reale!",
+	sendRequest: 'Trimiteti SOS Request'
+};
+
+window.locale_ro.bookings = {
+	title: "Rezervari",
+	notFound: "In prezent nu aveti rezervari curente",
+	pnr: "PNR",
+	plate: "Targa",
+	color: "Culoare",
+	seats: "Locuri",
+	externalClean: "extern",
+	internalClean: "Interni",
+	primi: "Retragere",
+	set: "Revenire",
+	openCar: "Incepeti sa rulati",
+	closeCar: "Sfarsitul rundei",
+	cancelReservation: 'Anulare'
+}
+
+window.locale_ro.parking = {
+	title: "Alegeti parcarea",
+	noparking: "Nu sa gasit parcare",
+	placeholderCautare: "Cautare"
 };
 
 window.locale_ro.tabs = {
-	reservations: 'Reservations',
-	park: 'Park',
-	booking: 'Booking',
-	vehicles: 'Vehicles',
-	settings: 'Settings'
+	reservation: "Rezervari",
+	park: "Parkings",
+	reserve: "Rezervati",
+	vehicles: "Vehicule",
+	settings: "Setari",
+	sos: "SOS",
+	notifications: "Notificari"
 };
 
-window.locale_ro.home = {
-	msgHello: "Hello, {0}",
-	msgNoRsv: 'You have no reservation in progress',
-	msgNextRsv: 'Hai una prenotazione il {0} alle ore {0} ',
-	msgOnRsv : "Devi riconsegnare l'auto alle ore {0}",
-	labelCar: 'Vehicle',
-	labelGet: 'Pick-Up',
-	labelSet: 'Return',
-	butBooking: 'New booking',
-	labelCountdown: 'Countdown',
-	labelPlate: 'Plate',
-	cancelReservation: 'Cancel',
-	labelColor: 'Color',
-	confirmDeleteBook: 'Revoke selected booking?',
-	title: 'Reservari',
-	prenotationtitle: 'Booking detail',
-	vehicle_position: 'Vehicle position address'
+window.locale_ro.notifications = {
+	title: "Notificari",
+	notFound: "Nu exista notificari .."
 };
 
-window.locale_ro.home.rsvinfo = {
-	labPRN: "PRN ",
-	labPrk: 'Parking ',
-	labTime: '',
+window.locale_ro.commons = {
+	refresh: "Actualizare ...",
+	yes: "Da",
+	no: "Nu",
+	legalNotice: "Drepturi de autor © 2016 Corporate Solutions Italia Toate drepturile rezervate",
+	readMore: 'Continue reading ...',
+	search: 'Cautare ..',
+	select: 'Selectati',
+	close: "Inchideti",
+	attention: "Atentie",
+	insert: "Introduceti",
+	alert: "Alert",
+	succes: "Succes",
+	error: "Eroare",
+	connectionProblem: "Am intampinat o problema de conectare!",
+	succesOperatie: "Operatiunea de succes",
+	requestFail: "Nu am putut trimite cererea.",
+	refuse: "Respingeti",
+	accept: "Accept"
+};
+
+window.locale_ro.bookResume = {
+	title: "Carte",
+	parking: "Parcare",
+	parkOpenedFrom: 'Deschis de la',
+	parkOpenedTo: "Alle",
+	parkOpened24h: 'Open H24',
+	dateFrom: 'data colectarii',
+	dateTo: "Data livrarii",
+	cc: "Cardul de carburant",
+	cc_descr: "Selectati daca aveti nevoie de cartea de combustibil",
+	drivingRange: "Raza de calatorie",
+	telepass: "Telepass",
+	telepass_descr: "Selectati daca aveti nevoie de telepass",
+	vahicleType: "Tipul vehiculului",
+	confirm: 'Cautati vehicul',
+	returnDateIsOut: "Data colectarii este in afara orelor de parcare",
+	returnDateIsMajor: "Data colectarii este mai mare decat data livrarii",
+	returnDateNeedToBeMajor: "Data colectarii trebuie sa fie mai mare decat data curenta",
+	isNotInTime: "Data livrarii este in afara orelor de parcare",
+	defineRange: "Nu ati definit raza de calatorie",
+	missingVehicleType: "Selectati un tip de vehicul",
+	vehicleTypesNotFound: "Nu a fost posibila recuperarea tipurilor de vehicule din acest parc auto!",
+	noDataFound: "Unele informatii nu au putut fi preluate!",
+	returnDateHaveToBe: "Data de intoarcere trebuie sa fie inainte",
+	wrongDates: "Selectati data colectarii si data livrarii pentru a continua",
+	wrongParking: "Selectati parcarea inainte de a continua"
+};
+
+window.locale_ro.map = {
+	title: "Detaliile rezervarii",
+	carLocation: "Adresa pozitiei vehiculului",
+	primi: "Retragere",
+	set: "Revenire",
+	goTo: "Reach vehicul",
+	stergeti: "Ștergeti",
+};
+
+window.locale_ro.confirmReservation = {
+	title: "Confirmarea rezervarii",
+	inapoi: "Inapoi",
+	confirmati: "Confirmati",
+	asteptati: "asteptati ..",
+	requestComplete: "Rezervare confirmata",
+	locuri: "Locuri",
+	culoare: "Culoare",
+	incompleta: "Atentie",
+	parc: "Parcare",
+	selectPlace: "Introduceti Locul",
+	selectDriveName: "Introduceti numele soferului",
+	dateFrom: 'data colectarii',
+	dataTo: "Data livrarii",
+	cc: "Cardul de carburant",
+	telepass: "Telepass",
+	pnr: "PNR"
+};
+
+window.locale_ro.vehicle = {
+	title: "Selecteaza vehicul",
+	notFound: "Ne pare rau, toate vehiculele sunt rezervate pentru perioada pe care ati solicitat-o.",
+	placa: "Targa",
+	selectati: 'Selectati',
+	locuri: "Locuri",
+	culoare: "Culoare",
+	externClean: "extern",
+	internClean: "Interni",
+	labelCannotReserve: "Rezervarea dvs. nu poate fi acceptata, deoarece exista deja o rezervare in numele dvs. pentru perioada solicitata."
 };
 
 window.locale_ro.login = {
+	username: "Utilizator",
+	parola: 'Parola',
 	login: 'Login',
-	forgotPassword: 'Forgot password?',
-	retrievePassword: 'Retrieve Password',
-	haveAccount: 'I have account',
-	placeholderMail: 'company@domain.com'
+	uitatPassword: "Parola uitata?",
+	retrievePassword: 'Recuperare parola',
+	haveAccount: 'Login',
+	email: 'Email',
+	placeholderEmail: "compagnia@dominio.it",
+	companyCode: "Verificati codul companiei",
+	companyCodePlaceholder: 'Introduceti codul companiei'
 };
 
-window.locale_ro.parking = {
-	button: 'Next',  
-	labelSearch: 'Search parking..',
-	labelSelectParking: 'Select parking',
-	noparking: 'No parkings found',
-	placeholderSearch: 'Search'
+window.locale_ro.settings = {
+	personalData: "Date personale",
+	client: "Client",
+	title: "Setari",
+	check1: 'Minimize Images',
+	check1_descr: "Afiseaza imaginile reduse ale vehiculului",
+	editPassword: "Schimbati parola"
 };
 
-
-window.locale_ro.vehicle = {
-	buttonBook:'Reserve',
-	buttonConfirm: '',
-	title: 'Vehicles',
-	messageEnpty: 'No available vehicles',
-	label: 'Vehicle',
-	labelPlate: 'Plate',
-	labelMotor: 'Motor',
-	labelSeats: 'Seats',
-	labelExternal: 'Exterior',
-	labelInterior: 'Interior',
-	labelColor: 'Color',
-	yes: 'YES',
-	no: 'NO',
-	labelSelect: "Select",
-    labelCannotReserve: 'Your booking request cannot be accepted, due to an existing one in the same period at your name.'
+window.locale_ro.driveLicense = {
+	title: "Permis de conducere",
+	licenta: "Licenta",
+	numarul: "Numarul licentei",
+	issuingCountry: "Locul emiterii",
+	issuingDate: 'Data de lansare',
+	expiringDate: "Data de expirare",
+	editLicense: "Modificati datele de brevet",
+	cancel: "Anulati",
+	save: 'Salvati'
 };
 
-window.locale_ro.booking = {
-	buttonDate: 'Change',
-	buttonParking: 'Change',
-	messageEmptyDate: 'There is something wrong in date.',
-	buttonConfirm: 'Search Vehicle',
-	dateFrom : 'Pickup date',
-	dateTo: 'Return date',
-	alertMessage: 'Select Data Properly',
-	parking: 'Parking',
-	justification: 'Justification',
-	place: 'Place',
-	cc: 'Fuel card',
-	telepass: 'Telepass',
-	telepass_descr: 'Select telepass',
-	labelDelete: 'Revoke booking',
-	wrongInputs: 'Please select correctly pick-up, return date and parking',
-	header: "Select ",
-	drivingRange: 'Driving range',
-	driverName: 'Driver name',
-	labelNavigate: 'Reach vehicle',
-	buttonDetail: 'Details',
-	buttonOpenCar : 'Open',
-	buttonCloseCar : 'Close',
-	labelCentralizzedButton: "Open/Close Vehicle"
+window.locale_ro.editPassword = {
+	title: "Schimbarea parolei",
+	currentPassword: 'Introduceti parola curenta',
+	newPassword: 'Introduceti o noua parola',
+	repeatPassword: "Confirmati o noua parola",
+	cancel: "Anulati",
+	edit: 'Edit'
 };
 
-window.locale_ro.confirmation = {
-	title: 'Confirm reservation',
-	back: 'Cancel',
-	buttonConfirm: 'Confirm',
-	labelWait: 'please wait..',
-	labelRequestComplete: 'Reservation confirmed',
-	labelSeat: "Seats",
-	labelColor: "Color",
-	labelIncomplete: 'Attention',
-	labelSelectPlace: "Insert 'Place'",
-	labelSelectDriveName: "Insert 'Driver name'"
+window.locale_ro.contacts = {
+	title: "Contacte",
+	email: 'Email',
+	phone: 'Telefon',
+	orari: "ore"
 };
 
-window.locale_ro.date = {
-	startDate: 'Pick-Up date',
-	startTime: 'Pick-Up time',
-	endDate: 'Return date',
-	endTime: 'Return time',
-	button: 'Next',
-	change: 'Change',
-	messageDate: 'Please select date Properly',
-	messageTime: 'Please select time Properly',
-	messageAlert: 'Please select Properly',
-	butChange: 'Change',
-	labelSelectRange: 'Select booking date range',
-	labelIncorrectDate: 'Date is not correct',
-	labelSmallDate: "Pick-Up Time can't be less then current time",
-	labelToday: "Today",
-	labelClose: "Close",
-	monthList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
-	weekDaysList: ["Sun", "Mon", "Tue", "Wed", "thu", "Fri", "Sat"]
+window.locale_ro.clientDetails = {
+	title: "Date personale",
+	anagrafic: "Anagrafica",
+	firstName: "Nume",
+	lastName: 'Nume de familie',
+	username: "Utilizator",
+	badge: "BadgeID",
+	contacts: 'Contacte',
+	email: 'Email',
+	phone: 'Mobile',
+	editContacts: 'Editati contactele',
+	cancel: "Anulati",
+	save: 'Salvati'
 };
 
-window.locale_ro.account = {
-	title: 'Settings',
-	check1: 'Small list',
-	client: 'Client',
-	logout: 'Logout',
-	check1_descr: 'Show list with small pictures'
+window.locale_ro.actionSheet = {
+	chageDriver: "Ghid de schimbare",
+	selectEmployee: 'selectati angajatul',
+	confirmati: "Confirmati",
+	cancel: "Anulati",
+	continuati: "Continuati",
+	Raportarea: "Raportarea",
+	atentie: "Atentie",
+	damageInfo1: "Efectuati Raportarea defectiunilor, numai dupa ce ati constatat ca vehiculul nu este in masura sa se intoarca la parcare si trebuie sa fie adus sau transferat intr-un magazin de reparatii.",
+	damageInfo2: "Raportarea defectiunilor implica, prin urmare, inchiderea cursei actuale, pentru a le permite sa rezerve un alt vehicul, daca se doreste. Pentru a incheia aceasta cursa, este solicitata sa comunice in plus ",
+	damageInfo3: "Va rugam sa retineti ca odata ce a fost raportata defectiunea, vehiculul nu va mai fi utilizabil",
+	totalKM: "Total kilometri afisati pe tabloul de bord",
+	noteWithDesc: "Note (utile pentru diagnosticarea problemei)",
+	needSupport: "Daca vehiculul trebuie remorcat sau nu",
+	note: "Note",
+	chooseDefectType: "Alege tipul de defect",
+	fuelLevel: "Selectati nivelul combustibilului din rezervor",
+	notesPlaceholder: "Informatii utile",
+	kmPlaceholder: 'Tastati kilometrajul',
+	mechanicalinc: "Vehicul remorcat in atelier?"
 };
-
-window.locale_ro.resume = {
-	title: 'Resume',
-	wrongDates: 'Select pick-up and return date to continue',
-	wrongParking: 'Select parking to continue'
-};
-
