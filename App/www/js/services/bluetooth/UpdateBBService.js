df6e43ebfe5f6eb103ea85cdd8270b71a1680d44
Alex <ouatu.alexandru@gmail.com>
2019-06-16 02:15:16 +0200

    avanzamento update bb

--- App/www/js/services/bluetooth/UpdateBBService.js
+++ App/www/js/services/bluetooth/UpdateBBService.js
@@ -9,6 +9,10 @@ angular.module('starter').service("UpdateBBService", function($q, $rootScope, Sc
         localStorage.setItem('updateBB', TKN);
     }
 
+    function getExistingToken() {
+        return localStorage.getItem('updateBB');
+    }
+
     function deleteExistingRequest() {
         localStorage.removeItem(updateBB);
     }
@@ -16,6 +20,13 @@ angular.module('starter').service("UpdateBBService", function($q, $rootScope, Sc
     function updateBB() {
         return $q(function(resolve, reject) {
             if (checkIsExistingRequest()) {
+                var tknObj = JSON.parse(atob(getExistingToken));
+                console.log("tknObj", tknObj);
+                if (tknObj.ty === 0) {
+                    pickupUpdate(tknObj, resolve, reject);
+                } else {
+                    returnUpdate(tknObj, resolve, reject);
+                }
                 resolve();
                 console.log('i updateBB');
                 deleteExistingRequest();
@@ -24,6 +35,47 @@ angular.module('starter').service("UpdateBBService", function($q, $rootScope, Sc
             }
         });
         
+    }
+
+    function pickupUpdate(tknObj) {
+        ScriptServices.getXMLResource(522).then(function(res) {
+            res = res.replace('{PNR}', tknObj.rid)
+            .replace('{BADGEID}', tknObj.bid)
+            .replace('{DATA}', JSON.stringify(new Date(tknObj.et  * 1000 )))
+            .replace('{IDZB}', tknObj.pid)
+            .replace('{LAT}', tknObj.poi.geo.coordinates[0])
+            .replace('{LONG}', tknObj.poi.geo.coordinates[1])
+            .replace('{quality}', '100%');
+            ScriptServices.callGenericService(res, 522).then(function(data) {
+                console.log('updated');
+            }, function(error) {
+
+            });
+        });
+    }
+
+
+    function returnUpdate(tknObj) {
+
+        ScriptServices.getXMLResource(523).then(function(res) {
+            res = res.replace('{PNR}', tknObj.rid)
+            .replace('{BADGEID}', tknObj.pnr)
+            .replace('{DATA}', JSON.stringify(new Date(tknObj.et  * 1000 )))
+            .replace('{KM}', tknObj.pnr)
+            .replace('{FUEL}', tknObj.pnr)
+            .replace('{STATO}', tknObj.pnr)
+            .replace('{STATOOP}', tknObj.pnr)
+            .replace('{SLOT}', tknObj.pnr)
+            .replace('{IDZB}', tknObj.pnr)
+            .replace('{LAT}', tknObj.poi.geo.coordinates[0])
+            .replace('{LONG}', tknObj.poi.geo.coordinates[1])
+            .replace('{QUALITY}', tknObj.pnr);
+            ScriptServices.callGenericService(res, 523).then(function(data) {
+
+            }, function(error) {
+
+            });
+        });
     }
 
     return {
@@ -38,4 +90,4 @@ angular.module('starter').service("UpdateBBService", function($q, $rootScope, Sc
         },
     };
 
-});
+})
