5e72a37f0f6be7eb57395ae693e16689e96593c6
massimo lanera <maxtblack94@gmail.com>
2020-10-01 15:28:47 +0200

    avanzamenti finale

--- App/www/js/controllers/controllerEmployeeReservation.js
+++ App/www/js/controllers/controllerEmployeeReservation.js
@@ -4,11 +4,11 @@ angular.module('starter').controller('EmployeeReservationCtrl', function(ScriptS
 
     function getEmployee(params) {
         $ionicLoading.show();
-        ScriptServices.getXMLResource(552).then(function(res) {
+        ScriptServices.getXMLResource(705).then(function(res) {
             res = res.replace('{DRIVERNUMBER}', InfoFactories.getUserInfo().driverNumber)
             .replace('{MINVAL}', 0)
             .replace('{MAXVAL}', 25);
-            ScriptServices.callGenericService(res, 552).then(function(data) {
+            ScriptServices.callGenericService(res, 705).then(function(data) {
                 $ionicLoading.hide();
                 $scope.employeeReservationList = ((data.data || {}).AllBookingList || []);
             }, function(error) {
