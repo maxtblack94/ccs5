082a643c1b74d1fc037fe3c49c423f9f0af56b7c
massimo lanera <maxtblack94@gmail.com>
2020-10-07 14:04:51 +0200

    avanzamento

--- App/www/js/controllers/controllerAboutUs.js
+++ App/www/js/controllers/controllerAboutUs.js
@@ -1,19 +1,4 @@
 angular.module('starter').controller('aboutUsCtrl', function( $scope, InfoFactories) {
     $scope.selectedClient = InfoFactories.getClientSelected();
-  $scope.selectedClient.aboutInfo =  {
-    title: "GPP d.o.o. - Gradski prijevoz putnika, društvo s ograničenom odgovornošću",
-    infoList : [{
-       label: "Adresa",
-       value: "Cara Hadrijana 1, 31000 Osijek"
-    },{
-       label: "MB",
-       value: "3026132"
-    },{
-        label: "OIB",
-        value: "96779488329"
-     },{
-        label: "CL",
-        value: "0800 7555"
-     }]
-}
+  
 })
